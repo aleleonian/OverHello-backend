0612ddc1dd0dc946167efd26e187a83710bece1b
Alejandro Leonian <alejandromleonian@gmail.com>
2023-11-04 17:13:47 -0500

    fixed a CORS issue

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors')
+
+var corsOptions = {
+  origin: process.env.CORS_HOST,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post("/", cors(corsOptions), function (req, res, next) {
+  console.log(req.body);
+  const data = req.body;
+  res.sendStatus(200);
+})
 module.exports = router;
